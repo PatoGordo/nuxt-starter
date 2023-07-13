425d8eba4bdf3fc4141c9b983e882d9b1cef843f
IcaroMiguel <conversecomicaro@gmail.com>
2023-07-13 17:18:53 -0300

    refacor: remove manual imported utils

--- server/api/auth/sign-up.post.ts
+++ server/api/auth/sign-up.post.ts
@@ -1,7 +1,45 @@
-import { handleResult } from "../../utils";
+import { z } from "zod";
+import { prismaClient } from "../../services";
+import bcrypt from "bcryptjs";
 
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
   try {
+    const body = await readBody(event);
+
+    const validation = z.object({
+      name: z.string().min(1),
+      email: z.string().email(),
+      password: z.string().min(8).max(255),
+    });
+
+    validation.parse(body);
+
+    const emailAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: body.email,
+      },
+    });
+
+    if (emailAlreadyExists) {
+      throw new HTTPException({
+        message: "Email already in use! Use another or login with this email.",
+      });
+    }
+
+    const passwordHash = bcrypt.hashSync(body.password, 12);
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: body.name,
+        email: body.email,
+        password: passwordHash,
+      },
+    });
+
+    const linking = "";
+
+    const bearerToken = "";
+
     return handleResult({
       ok: true,
     });
--- server/api/feature/index.get.ts
+++ server/api/feature/index.get.ts
@@ -1,5 +1,3 @@
-import { handleResult } from "../../utils";
-
 export default defineEventHandler((event) => {
   try {
     return handleResult({
