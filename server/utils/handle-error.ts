02ae386f8c296e0709e45dea18e7198e2b27fff8
IcaroMiguel <conversecomicaro@gmail.com>
2023-07-13 17:20:51 -0300

    refactor: add default error handling

--- server/utils/handle-error.ts
+++ server/utils/handle-error.ts
@@ -7,6 +7,17 @@ export const handleError = (event: H3Event, err: unknown) => {
     return {
       ...err,
       path: getRequestPath(event),
+      handled: true,
+    };
+  } else {
+    setResponseStatus(event, 400);
+
+    return {
+      message: (err as Error).message,
+      error: err as Error,
+      timestamp: new Date(),
+      path: getRequestPath(event),
+      handled: true,
     };
   }
 };
