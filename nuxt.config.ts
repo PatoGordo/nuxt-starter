c857a861fc4744439e346cd42b2c21593691dd50
IcaroMiguel <conversecomicaro@gmail.com>
2023-07-13 14:32:33 -0300

    feat: add nuxt security and cors policy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,21 @@
 export default defineNuxtConfig({
+  modules: ["nuxt-security"],
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
       CONFIG_CAT_SDK_KEY: process.env.CONFIG_CAT_SDK_KEY,
+    },
+  },
+  security: {
+    corsHandler: {
+      origin: "*",
+      methods: "*",
+      allowHeaders: "*",
+      exposeHeaders: "*",
+    },
+    headers: {
+      xXSSProtection: "1",
+      contentSecurityPolicy: false,
     },
   },
 });
