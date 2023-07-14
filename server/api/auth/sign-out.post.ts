b446eae8b23b8986bff89a4b4bdcb16e40ae03e2
IcaroMiguel <conversecomicaro@gmail.com>
2023-07-14 15:31:22 -0300

    feat: implements the sign-out api route

--- server/api/auth/sign-out.post.ts
+++ server/api/auth/sign-out.post.ts
@@ -1,8 +1,25 @@
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
   try {
-    return handleResult({
-      ok: true,
+    const link = event.context.token_data.link;
+
+    const session = await prismaClient.session.findFirst({
+      where: {
+        link,
+      },
+      select: {
+        id: true,
+      },
     });
+
+    if (session) {
+      await prismaClient.session.delete({
+        where: {
+          id: session.id,
+        },
+      });
+    }
+
+    return handleResult({});
   } catch (error) {
     return handleError(event, error);
   }
