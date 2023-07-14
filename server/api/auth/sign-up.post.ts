32dac04068c59e78ffc2d232b77efca2c9bcd92e
IcaroMiguel <conversecomicaro@gmail.com>
2023-07-14 15:31:15 -0300

    feat: implements the sign-up api route

--- server/api/auth/sign-up.post.ts
+++ server/api/auth/sign-up.post.ts
@@ -1,16 +1,21 @@
 import { z } from "zod";
 import { prismaClient } from "../../services";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import moment from "moment";
+import { generateID } from "../../utils/generate-id";
 
 export default defineEventHandler(async (event) => {
   try {
     const body = await readBody(event);
 
-    const validation = z.object({
-      name: z.string().min(1),
-      email: z.string().email(),
-      password: z.string().min(8).max(255),
-    });
+    const validation = z
+      .object({
+        name: z.string().min(1),
+        email: z.string().email(),
+        password: z.string().min(8).max(255),
+      })
+      .strict();
 
     validation.parse(body);
 
@@ -22,7 +27,7 @@ export default defineEventHandler(async (event) => {
 
     if (emailAlreadyExists) {
       throw new HTTPException({
-        message: "Email already in use! Use another or login with this email.",
+        message: "Email already in use! Use another or login with this one.",
       });
     }
 
@@ -36,12 +41,49 @@ export default defineEventHandler(async (event) => {
       },
     });
 
-    const linking = "";
+    const link = generateID(24);
 
-    const bearerToken = "";
+    const bearerToken = jwt.sign(
+      {
+        user_id: user.id,
+        link,
+      },
+      String(process.env.BEARER_TOKEN_JWT_SECRET),
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    const refreshToken = jwt.sign(
+      {
+        link,
+      },
+      String(process.env.REFRESH_TOKEN_JWT_SECRET),
+      {
+        expiresIn: "3d",
+      }
+    );
+
+    await prismaClient.session.create({
+      data: {
+        link,
+        user_agent: getRequestHeader(event, "User-Agent") || "N/A",
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+        expires_at: moment().add(3, "d").toDate(),
+      },
+    });
 
     return handleResult({
-      ok: true,
+      token: bearerToken,
+      refresh_token: refreshToken,
+      user: {
+        ...user,
+        password: "protected-data",
+      },
     });
   } catch (error) {
     return handleError(event, error);
