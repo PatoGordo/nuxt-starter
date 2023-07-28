ff143233cc0c9aa0873a59ba908b19c904f37aa3
IcaroMiguel <conversecomicaro@gmail.com>
2023-07-28 13:04:52 -0300

    feat: add routes to user change data and to delete the account

--- server/api/users/me/index.delete.ts
+++ server/api/users/me/index.delete.ts
@@ -1,8 +1,51 @@
-export default defineEventHandler((event) => {
+import { z } from "zod";
+import bcrypt from "bcryptjs";
+
+export default defineEventHandler(async (event) => {
   try {
-    return handleResult({
-      ok: true,
+    const userId = event.context.user.id;
+
+    const body = await readBody(event);
+
+    const validation = z
+      .object({
+        password: z.string().min(8).max(255),
+      })
+      .strict();
+
+    validation.parse(body);
+
+    const user = await prismaClient.user.findFirst({
+      where: {
+        id: userId,
+      },
     });
+
+    if (!user) {
+      return handleResult({});
+    }
+
+    const isPasswordTheSame = bcrypt.compareSync(body.password, user?.password);
+
+    if (!isPasswordTheSame) {
+      throw new HTTPException({
+        message: "Password is incorrect!",
+      });
+    }
+
+    validation.parse(body);
+
+    await prismaClient.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        deleted: true,
+        email: "!deleted!" + user.email,
+      },
+    });
+
+    return handleResult({});
   } catch (error) {
     return handleError(event, error);
   }
--- server/api/users/me/index.put.ts
+++ server/api/users/me/index.put.ts
@@ -1,8 +1,29 @@
-export default defineEventHandler((event) => {
+import { z } from "zod";
+
+export default defineEventHandler(async (event) => {
   try {
-    return handleResult({
-      ok: true,
+    const userId = event.context.user.id;
+
+    const body = await readBody(event);
+
+    const validation = z
+      .object({
+        name: z.string().optional(),
+      })
+      .strict();
+
+    validation.parse(body);
+
+    await prismaClient.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        ...body,
+      },
     });
+
+    return handleResult({});
   } catch (error) {
     return handleError(event, error);
   }
