fe7bd92cfcda0b3a67a5d32da0fe1c99be7b780e
patogordo <conversecomicaro@gmail.com>
2023-10-22 14:45:44 -0300

    refactor: remove emailing password change on forgot password

--- server/api/v1/auth/reset-password.patch.ts
+++ server/api/v1/auth/reset-password.patch.ts
@@ -1,13 +1,9 @@
 import { z } from "zod";
 import bcrypt from "bcryptjs";
-import { appConfig } from "~/config/app";
 
 export default defineEventHandler(async (event) => {
   try {
     const body = await readBody(event);
-
-    const url = getRequestHost(event);
-    const protocol = getRequestProtocol(event);
 
     const validation = z
       .object({
@@ -39,7 +35,7 @@ export default defineEventHandler(async (event) => {
 
     const newPasswordHash = bcrypt.hashSync(body.new_password, 12);
 
-    const res = await prismaClient.passwordChange.update({
+    await prismaClient.passwordChange.update({
       where: {
         id: passwordChangeRequest.id,
       },
@@ -59,21 +55,6 @@ export default defineEventHandler(async (event) => {
           },
         },
       },
-    });
-
-    const emailTemplate = `
-    <h2>Your password has been changed!</h2>
-    <p>If it was not you, use the link below to revert it.</p>
-    <p>This link will expires in seven days(7d).</p>
-    <br />
-    <a href="${protocol}://${url}/auth/revert-password-change?token=${body.token}">Revert password change password</a>
-    `;
-
-    await mailTransporter.sendMail({
-      to: res.user.email,
-      from: process.env.MAIL_USERNAME,
-      subject: `${appConfig.appName} - Your password has been changed`,
-      html: emailTemplate,
     });
 
     return handleResult({
